103d1c4d96e6d2bfda68541e56b613592cfe31f0
aurelien <aurelien.dauchot@soprism.com>
2024-05-16 11:25:49 +0200

    Tests for 4 and 5 to roman symbol

--- roman-converter.test.ts
+++ roman-converter.test.ts
@@ -3,7 +3,14 @@ import {RomanNumberConverter} from "./roman-converter";
 describe('RomanConverter', () => {
     it('should convert 1 to I', () => {
         const romanSymbol = RomanNumberConverter.convert(1);
-
         expect(romanSymbol).toBe('I');
+    });
+
+    it ('should convert 4 to IV', () => {
+       expect(RomanNumberConverter.convert(4)).toBe('IV');
+    });
+
+    it ('should convert 5 to V', () => {
+        expect(RomanNumberConverter.convert(5)).toBe('V');
     });
 });
--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,7 @@
 export abstract class RomanNumberConverter {
     static convert(number: number) {
         if (number == 1) return 'I';
+        if (number == 4) return 'IV';
+        if (number == 5) return 'V';
     }
 }
