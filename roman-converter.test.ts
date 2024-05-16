c0ac75ee0c9857c62f51aca7b2eb58b20e8be481
aurelien <aurelien.dauchot@soprism.com>
2024-05-16 11:50:10 +0200

    Conversion of arabic numbers from 1 to 10

--- roman-converter.test.ts
+++ roman-converter.test.ts
@@ -21,4 +21,24 @@ describe('RomanConverter', () => {
     it ('should convert 5 to V', () => {
         expect(RomanNumberConverter.convert(5)).toBe('V');
     });
+
+    it ('should convert 6 to VI', () => {
+        expect(RomanNumberConverter.convert(6)).toBe('VI');
+    });
+
+    it ('should convert 7 to VII', () => {
+        expect(RomanNumberConverter.convert(7)).toBe('VII');
+    });
+
+    it ('should convert 8 to VIII', () => {
+        expect(RomanNumberConverter.convert(8)).toBe('VIII');
+    });
+
+    it ('should convert 9 to IX', () => {
+        expect(RomanNumberConverter.convert(9)).toBe('IX');
+    });
+
+    it ('should convert 10 to X', () => {
+        expect(RomanNumberConverter.convert(10)).toBe('X');
+    });
 });
--- roman-converter.ts
+++ roman-converter.ts
@@ -4,7 +4,12 @@ export abstract class RomanNumberConverter {
         [2, 'II'],
         [3, 'III'],
         [4, 'IV'],
-        [5, 'V']
+        [5, 'V'],
+        [6, 'VI'],
+        [7, 'VII'],
+        [8, 'VIII'],
+        [9, 'IX'],
+        [10, 'X'],
     ]);
 
     static convert(number: number) {
