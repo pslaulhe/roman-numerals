f0176b2a4e93e6145fe15afa84d76f84086b7f95
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:07:51 +0200

    II Green

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,8 @@
 export abstract class RomanNumberConverter {
     static convert(number: number): string {
+        if (number === 2) {
+            return 'II';
+        }
         return 'I';
     }
 }
