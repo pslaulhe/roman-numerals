9051d33e452d4ecaa1680bdacab5cf3a3dc1ddcb
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:08:46 +0200

    IV Red

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,8 @@
 export abstract class RomanNumberConverter {
     static convert(number: number): string {
+        if (number === 4) {
+            return 'IV';
+        }
         if (number === 3) {
             return 'III';
         }
