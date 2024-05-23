3851571ea11a0cfe2617be4999818cb2e059311d
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:12:34 +0200

    V Green

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,8 @@
 export abstract class RomanNumberConverter {
     static convert(number: number): string {
+        if (number >= 5) {
+            return 'V';
+        }
         if (number === 4) {
             return 'IV';
         }
