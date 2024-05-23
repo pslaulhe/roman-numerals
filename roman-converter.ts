faaec2de831100ed37884d40894f6d13e3ae42be
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:08:17 +0200

    III Green

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,8 @@
 export abstract class RomanNumberConverter {
     static convert(number: number): string {
+        if (number === 3) {
+            return 'III';
+        }
         if (number === 2) {
             return 'II';
         }
