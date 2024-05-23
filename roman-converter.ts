fc502a066fa5b715b40b064bd4f5364af2bb7a04
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:25:46 +0200

    >= 40 Green

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,5 +1,13 @@
 export abstract class RomanNumberConverter {
     static map = new Map<number, string>([
+        [1000, 'M'],
+        [900, 'CM'],
+        [500, 'D'],
+        [400, 'CD'],
+        [100, 'C'],
+        [90, 'XC'],
+        [50, 'L'],
+        [40, 'XL'],
         [10, 'X'],
         [9, 'IX'],
         [5, 'V'],
