574b99cfdac358d64f674a8785fe49f085ef4e42
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:17:46 +0200

    7, 8, 9 Refactor 6

--- roman-converter.ts
+++ roman-converter.ts
@@ -1,13 +1,13 @@
 export abstract class RomanNumberConverter {
     static convert(number: number): string {
         if (number >= 9) {
-            return 'IX';
+            return 'IX' + this.convert(number - 9);
         }
         if (number >= 5) {
             return 'V' + this.convert(number - 5);
         }
         if (number >= 4) {
-            return 'IV';
+            return 'IV' + this.convert(number - 4)
         }
         if (number >= 1) {
             return 'I' + this.convert(number - 1);
