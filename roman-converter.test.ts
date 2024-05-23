a7b83d1aa254c5dc556b65d3a177182b874e1b31
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:07:37 +0200

    II Red

--- roman-converter.test.ts
+++ roman-converter.test.ts
@@ -1,7 +1,11 @@
 import {RomanNumberConverter} from "./roman-converter";
 
 describe('RomanConverter', () => {
-   it('should convert 1 to I', () => {
-         expect(RomanNumberConverter.convert(1)).toBe('I');
-   });
+    it('should convert 1 to I', () => {
+        expect(RomanNumberConverter.convert(1)).toBe('I');
+    });
+
+    it('should convert 2 to II', () => {
+        expect(RomanNumberConverter.convert(2)).toBe('II');
+    });
 });
