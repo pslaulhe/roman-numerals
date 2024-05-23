5f48ec5f254cd998d7891e031dba8e299ec4d683
pslaulhe <pslaulhe@gmail.com>
2024-05-23 09:11:26 +0200

    IV Refactor 4

--- roman-converter.ts
+++ roman-converter.ts
@@ -3,13 +3,7 @@ export abstract class RomanNumberConverter {
         if (number === 4) {
             return 'IV';
         }
-        if (number === 3) {
-            return 'I' + this.convert(number - 1);
-        }
-        if (number === 2) {
-            return 'I' + this.convert(number - 1);
-        }
-        if (number === 1) {
+        if (number >= 1) {
             return 'I' + this.convert(number - 1);
         }
         return '';
